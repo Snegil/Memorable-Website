dff813d9c5925fc6eb8ec6fb4aa2da3b822bd65e
Sneg <144wallfin@gmail.com>
2023-02-23 18:41:43 +0100

    Popup fits screen better, and stops scrolling

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 function FastToggle(ID) {
 	var x = document.getElementById(ID);
+	var body = document.getElementsByName(body);
 	if (x.style.display !== "inline-block") {
 	x.style.display = "inline-block";
+	document.body.style.overflowY = "hidden";
 	} else {
 		x.style.display = "none";
+		document.body.style.overflowY = "auto";
 	}
 }
